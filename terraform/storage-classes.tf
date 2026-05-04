e348636310848250dd9ecb08d25b5f9ad771999b
Svyatoslav <silazare.nsk@gmail.com>
2026-05-04 10:42:28 +0700

    fix: move storage class to tf

---
+++ terraform/storage-classes.tf
@@ -0,0 +1,21 @@
+resource "kubernetes_storage_class_v1" "gp3" {
+  metadata {
+    name = "gp3"
+    annotations = {
+      "storageclass.kubernetes.io/is-default-class" = "true"
+    }
+  }
+
+  storage_provisioner    = "ebs.csi.aws.com"
+  volume_binding_mode    = "WaitForFirstConsumer"
+  reclaim_policy         = "Delete"
+  allow_volume_expansion = true
+
+  parameters = {
+    type      = "gp3"
+    fsType    = "ext4"
+    encrypted = "true"
+  }
+
+  depends_on = [module.eks]
+}
